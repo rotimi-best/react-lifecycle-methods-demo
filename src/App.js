700518e680440b9adc0367e7789ed209ef3b0f77
Rotimi Best <rotimiibitoyeemma@gmail.com>
2022-08-25 10:17:27 +0000

    Add routes

--- src/App.js
+++ src/App.js
@@ -2,43 +2,41 @@ import React from "react";
 import MostlyUsed from "./components/MostlyUsed";
 import RarelyUsed from "./components/RarelyUsed";
 import PracticeTask from "./components/PracticeTask";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
-function Header({ setStep }) {
-  const handleClick = step => {
-    setStep(step)
-  };
-
+function Header() {
   return (
     <nav>
       <ul>
-        <li onClick={() => handleClick(1)}>Most Used Method</li>
-        <li onClick={() => handleClick(2)}>Rarely Used Method</li>
-        <li onClick={() => handleClick(3)}>Practice task</li>
+        <Link to="/">Home</Link>
+        <Link to="/mostused">Most Used Method</Link>
+        <Link to="/rarelyused">Rarely Used Method</Link>
+        <Link to="/practicetask">Practice task</Link>
       </ul>
     </nav>
-  )
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome home guys</h2>
+    </div>
+  );
 }
 
 function App() {
-  const [step, setStep] = React.useState(1);
-
-  const renderRightComponent = () => {
-    switch (step) {
-      case 1:
-        return <MostlyUsed />;
-      case 2:
-        return <RarelyUsed />;
-      default:
-        return <PracticeTask />;
-    }
-  }
-
   console.clear();
   return (
     <div className="App">
-      <Header setStep={setStep} />
-      {renderRightComponent()}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="mostused" element={<MostlyUsed />} />
+        <Route path="rarelyused" element={<RarelyUsed />} />
+        <Route path="practicetask" element={<PracticeTask />} />
+      </Routes>
     </div>
   );
 }
